0d2c75c8693e4c714eb8436f7a1fa5f5b15c6c47
YuaCC <1095192336@qq.com>
2020-12-09 15:09:24 +0800

    run test results

--- run.sh
+++ run.sh
@@ -4,20 +4,20 @@ python galaxy.py 11 29 1 10 > anynet_file
 ./booksim2-master/src/booksim anynet_config_load06 > log/galaxy_11_29_1_10_load06_log.txt
 ./booksim2-master/src/booksim anynet_config_load08 > log/galaxy_11_29_1_10_load08_log.txt
 
-python galaxy.py 11 29 1 10 > anynet_file
-./booksim2-master/src/booksim anynet_config_load02 > log/galaxy_11_29_1_10_load02_log.txt
-./booksim2-master/src/booksim anynet_config_load04 > log/galaxy_11_29_1_10_load04_log.txt
-./booksim2-master/src/booksim anynet_config_load06 > log/galaxy_11_29_1_10_load06_log.txt
-./booksim2-master/src/booksim anynet_config_load08 > log/galaxy_11_29_1_10_load08_log.txt
+python galaxy.py 15 29 1 7 > anynet_file
+./booksim2-master/src/booksim anynet_config_load02 > log/galaxy_15_29_1_7_load02_log.txt
+./booksim2-master/src/booksim anynet_config_load04 > log/galaxy_15_29_1_7_load04_log.txt
+./booksim2-master/src/booksim anynet_config_load06 > log/galaxy_15_29_1_7_load06_log.txt
+./booksim2-master/src/booksim anynet_config_load08 > log/galaxy_15_29_1_7_load08_log.txt
 
-python galaxy.py 11 29 1 10 > anynet_file
-./booksim2-master/src/booksim anynet_config_load02 > log/galaxy_11_29_1_10_load02_log.txt
-./booksim2-master/src/booksim anynet_config_load04 > log/galaxy_11_29_1_10_load04_log.txt
-./booksim2-master/src/booksim anynet_config_load06 > log/galaxy_11_29_1_10_load06_log.txt
-./booksim2-master/src/booksim anynet_config_load08 > log/galaxy_11_29_1_10_load08_log.txt
+python galaxy.py 81 1 10 4 > anynet_file
+./booksim2-master/src/booksim anynet_config_load02 > log/galaxy_81_1_10_4_load02_log.txt
+./booksim2-master/src/booksim anynet_config_load04 > log/galaxy_81_1_10_4_load04_log.txt
+./booksim2-master/src/booksim anynet_config_load06 > log/galaxy_81_1_10_4_load06_log.txt
+./booksim2-master/src/booksim anynet_config_load08 > log/galaxy_81_1_10_4_load08_log.txt
 
-python galaxy.py 11 29 1 10 > anynet_file
-./booksim2-master/src/booksim anynet_config_load02 > log/galaxy_11_29_1_10_load02_log.txt
-./booksim2-master/src/booksim anynet_config_load04 > log/galaxy_11_29_1_10_load04_log.txt
-./booksim2-master/src/booksim anynet_config_load06 > log/galaxy_11_29_1_10_load06_log.txt
-./booksim2-master/src/booksim anynet_config_load08 > log/galaxy_11_29_1_10_load08_log.txt
+python galaxy.py 15 29 4 2 > anynet_file
+./booksim2-master/src/booksim anynet_config_load02 > log/galaxy_15_29_4_2_load02_log.txt
+./booksim2-master/src/booksim anynet_config_load04 > log/galaxy_15_29_4_2_load04_log.txt
+./booksim2-master/src/booksim anynet_config_load06 > log/galaxy_15_29_4_2_load06_log.txt
+./booksim2-master/src/booksim anynet_config_load08 > log/galaxy_15_29_4_2_load08_log.txt
